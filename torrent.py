6d7a9741a0300c476faf510bbe593f4c0b85a969
MurungaKibaara <murungakibaara@outlook.com>
2020-08-03 16:43:20 +0300

    [Refactor code]

--- torrent.py
+++ torrent.py
@@ -3,65 +3,46 @@ import fnmatch
 import os
 import time
 import sys
-import glob
 from upload import upload
-from clean import clean_files, clean_directories
 
+link = ('magnet:?xt=urn:btih:64A1872D2A1B4B12153C25FE29FAECAE0F19399C&dn=DJ+Khaled++POPSTAR+%28feat.+Drake%29+Rap+Single+2020+320+kbps+Beats%E2%AD%90&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce')
 
 def torrent():
+    '''Download a magnet link'''
 
-  ses = lt.session()
-  ses.listen_on(6881, 6891)
-  params = {'save_path': './Downloads'}
+    ses = lt.session()
+    ses.listen_on(6881, 6891)
+    params = {'save_path': './Downloads'}
 
-  localpath = './Downloads'
+    h = lt.add_magnet_uri(ses, link, params)
+    print("starting", h.name())
 
-  list_of_files = []
+    while not h.is_seed():
+        s = h.status()
 
+        state_str = [
+            "queued",
+            "checking",
+            "downloading metadata",
+            "downloading",
+            "finished",
+            "seeding",
+            "allocating",
+            "checking fastresume",
+        ]
+        print(
+            "\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s"
+            % (
+                s.progress * 100,
+                s.download_rate / 1000,
+                s.upload_rate / 1000,
+                s.num_peers,
+                state_str[s.state],
+            )
+        )
+        sys.stdout.flush()
+        time.sleep(1)
 
-  link = ('magnet:?xt=urn:btih:64A1872D2A1B4B12153C25FE29FAECAE0F19399C&dn=DJ+Khaled++POPSTAR+%28feat.+Drake%29+Rap+Single+2020+320+kbps+Beats%E2%AD%90&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce')
-  h = lt.add_magnet_uri(ses, link, params)
-  print("starting", h.name())
-
-  while not h.is_seed():
-      s = h.status()
-
-      state_str = [
-          "queued",
-          "checking",
-          "downloading metadata",
-          "downloading",
-          "finished",
-          "seeding",
-          "allocating",
-          "checking fastresume",
-      ]
-      print(
-          "\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s"
-          % (
-              s.progress * 100,
-              s.download_rate / 1000,
-              s.upload_rate / 1000,
-              s.num_peers,
-              state_str[s.state],
-          )
-      )
-      sys.stdout.flush()
-      time.sleep(1)
-
-  print(h.name(), "complete")
-
-  print('*'*60)
-  print('*'*60)
-
-
-
-  for root, dirnames, filenames in os.walk(localpath):
-    for filename in fnmatch.filter(filenames, '*'):
-        list_of_files.append(os.path.join(root, filename))
-  latest_file = max(list_of_files, key=os.path.getctime)
-  upload(latest_file)
-  clean_files()
-  clean_directories()
-
+    print(h.name(), "complete")
+    upload()
 torrent()
--- upload.py
+++ upload.py
@@ -1,21 +1,35 @@
 from mega import Mega
 from dotenv import load_dotenv, find_dotenv
 from os import getenv
-import glob
+import os
+import fnmatch
+from clean import clean_files, clean_directories
 
 load_dotenv(find_dotenv())
 
 EMAIL = getenv("EMAIL")
 PASSWORD = getenv("PASS")
 
-def upload(filename):
+def upload():
 
   mega = Mega()
   m = mega.login(EMAIL, PASSWORD)
 
+  localpath = './Downloads'
+
+  list_of_files = []
+
+  for root, dirnames, filenames in os.walk(localpath):
+    for filename in fnmatch.filter(filenames, '*'):
+        list_of_files.append(os.path.join(root, filename))
+  latest_file = max(list_of_files, key=os.path.getctime)
+
   folder = m.find('Downloads', exclude_deleted=True)
 
-  upload = m.upload(filename, folder[0])
+  upload = m.upload(latest_file, folder[0])
   upload_link = m.get_upload_link(upload)
+
+  clean_files()
+  clean_directories()
 
   print(upload_link)
